8bbfc3cb91060e357bc1a30ac7f40d0a8d84c3cf
Lampusxd <91846906+Lampusxd@users.noreply.github.com>
2022-01-28 18:34:50 -0500

    Hood Subsystem and Commands

---
+++ src/main/java/frc/robot/HoodOutCommand.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class HoodOutCommand extends CommandBase {
+  private HoodSubsystem hoodSubsystem; 
+  
+  /** Creates a new HoodIn. */
+  public HoodOutCommand(HoodSubsystem hoodSubsystem) {
+    this.hoodSubsystem = new HoodSubsystem();
+    addRequirements(hoodSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+ 
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(!(hoodSubsystem.getEncoder() -Constants.HOOD_ANGLE_TOLERANCE >= Constants.MAX_HOOD_ANGLE)){
+      hoodSubsystem.movehood(-Constants.HOOD_SPEED);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    hoodSubsystem.stophood(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
